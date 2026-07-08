1aad07a1fbb2e1025dba74a6fdb6786f78ea5532
TeapotExpress <swimsinvoid@gmail.com>
2026-07-08 02:28:04 +0200

    fix: pickable object attachment detection

--- Assets/Game/Scripts/XRGrabInterctableTwoAttachment.cs
+++ Assets/Game/Scripts/XRGrabInterctableTwoAttachment.cs
@@ -6,17 +6,22 @@ public class XRGrabInteractableTwoAttachment : XRGrabInteractable
 {
     public Transform LeftAttachmentTransform;
     public Transform RightAttachmentTransform;
-    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    
+    
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
     {
+        
         if (args.interactorObject.transform.CompareTag("Left Hand"))
         {
+            Debug.Log("Left Hand Attached");
             attachTransform = LeftAttachmentTransform;
         }
         else if (args.interactorObject.transform.CompareTag("Right Hand"))
         {
+            
             attachTransform = RightAttachmentTransform;
         }
-        base.OnSelectEntered(args); 
+        base.OnSelectEntering(args); 
     }
 }
 
