e0083a228e527d7b34cfeb7af753fa406eaee0e7
TeapotExpress <swimsinvoid@gmail.com>
2026-07-10 16:23:07 +0200

    refactor: make XRGrabInteractableTwoAttach handle socket attachments

--- Assets/Game/Scripts/XRGrabInterctableTwoAttachment.cs
+++ Assets/Game/Scripts/XRGrabInterctableTwoAttachment.cs
@@ -1,27 +1,23 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class XRGrabInteractableTwoAttachment : XRGrabInteractable
+public class XRGrabInteractableRoutedAttach : XRGrabInteractable
 {
     public Transform LeftAttachmentTransform;
     public Transform RightAttachmentTransform;
-    
-    
+    public Transform SocketAttachmentTransform;   // if left empty, act as two pose attachment
+
     protected override void OnSelectEntering(SelectEnterEventArgs args)
     {
-        
-        if (args.interactorObject.transform.CompareTag("Left Hand"))
-        {
-            Debug.Log("Left Hand Attached");
+        if (args.interactorObject is XRSocketInteractor && SocketAttachmentTransform != null)
+            attachTransform = SocketAttachmentTransform;
+        else if (args.interactorObject.transform.CompareTag("Left Hand"))
             attachTransform = LeftAttachmentTransform;
-        }
         else if (args.interactorObject.transform.CompareTag("Right Hand"))
-        {
-            
             attachTransform = RightAttachmentTransform;
-        }
-        base.OnSelectEntering(args); 
+
+        base.OnSelectEntering(args);
     }
 }
-
